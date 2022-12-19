7204c3b7043df46022ce725b1684a3ee9e33acd4
jhs512 <jangka512@gmail.com>
2022-12-19 11:21:06 +0900

    messages filtering

--- src/main/java/com/ll/exam/app_2022_12_19__1/ChatController.java
+++ src/main/java/com/ll/exam/app_2022_12_19__1/ChatController.java
@@ -1,12 +1,15 @@
 package com.ll.exam.app_2022_12_19__1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
+@Slf4j
 @RequestMapping("/chat")
 public class ChatController {
     private List<ChatMessage> chatMessages = new ArrayList<>();
@@ -31,16 +34,36 @@ public class ChatController {
         );
     }
 
+    public record MessagesRequest(Long fromId) {
+    }
+
     public record MessagesResponse(List<ChatMessage> messages, long count) {
     }
 
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<MessagesResponse> messages() {
+    public RsData<MessagesResponse> messages(MessagesRequest req) {
+        List<ChatMessage> messages = chatMessages;
+
+        // 번호가 입력되었다면
+        if (req.fromId != null) {
+            // 해당 번호의 채팅메세지가 전체 리스트에서의 배열인덱스 번호를 구한다.
+            // 없다면 -1
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 있다면, 0번 부터 index 번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
         return new RsData<>(
                 "S-1",
                 "성공",
-                new MessagesResponse(chatMessages, chatMessages.size())
+                new MessagesResponse(messages, messages.size())
         );
     }
 }
